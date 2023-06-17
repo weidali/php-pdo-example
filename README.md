ed1a4e58e22fc6b38337cd49c408ebc87fc30634
Vitaliy <vito.co@ya.ru>
2023-06-17 23:35:01 +0300

    - db connection

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # php-pdo-example
+
+## Installation
+```bash
+git clone git@github.com:weidali/php-pdo-example.git
+cd php-pdo-example
+composer install
+cp .env.example .env
+php -S localhost:8080
+```
--- db.php
+++ db.php
@@ -5,8 +5,21 @@ require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+$db_connection = $_ENV['DB_CONNECTION'];
 $host = $_ENV['DB_HOST'];
 $dbname = $_ENV['DB_DATABASE'];
 $port = $_ENV['DB_PORT'];
 $username = $_ENV['DB_USERNAME'];
 $password = $_ENV['DB_PASSWORD'];
+
+if (!in_array($db_connection, PDO::getAvailableDrivers())) {
+	echo "Error: Driver for db connection is not define";
+}
+
+try {
+	$pdo = new PDO("$db_connection:host=$host;dbname=$dbname;port=$port;", $username, $password);
+	return $pdo;
+} catch (PDOException $exception) {
+	echo "Error DB connection: {$exception->getMessage()}";
+	return null;
+}
