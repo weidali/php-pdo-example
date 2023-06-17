64e7953ebade7b113b5d6b65a8053f66f7904723
Vitaliy <vito.co@ya.ru>
2023-06-18 00:04:17 +0300

    - refactoring

--- db.php
+++ db.php
@@ -2,24 +2,38 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+function dbConnect(): PDO|null
+{
+	$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+	$dotenv->load();
 
-$db_connection = $_ENV['DB_CONNECTION'];
-$host = $_ENV['DB_HOST'];
-$dbname = $_ENV['DB_DATABASE'];
-$port = $_ENV['DB_PORT'];
-$username = $_ENV['DB_USERNAME'];
-$password = $_ENV['DB_PASSWORD'];
+	$db_connection = $_ENV['DB_CONNECTION'];
+	$host = $_ENV['DB_HOST'];
+	$dbname = $_ENV['DB_DATABASE'];
+	$port = $_ENV['DB_PORT'];
+	$username = $_ENV['DB_USERNAME'];
+	$password = $_ENV['DB_PASSWORD'];
 
-if (!in_array($db_connection, PDO::getAvailableDrivers())) {
-	echo "Error: Driver for db connection is not define";
-}
+	if (!in_array($db_connection, PDO::getAvailableDrivers())) {
+		echo "Error: Driver for db connection is not define";
+		return null;
+	}
 
-try {
-	$pdo = new PDO("$db_connection:host=$host;dbname=$dbname;port=$port;", $username, $password);
+	try {
+		$dsn = "$db_connection:host=$host;dbname=$dbname;port=$port;";
+		$pdo = new PDO(
+			$dsn,
+			$username,
+			$password,
+			[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+		);
+
+		if ($pdo) {
+			echo "Connected to the $dbname database successfully!";
+		}
+	} catch (PDOException $e) {
+		die($e->getMessage());
+	}
 	return $pdo;
-} catch (PDOException $exception) {
-	echo "Error DB connection: {$exception->getMessage()}";
-	return null;
 }
+return dbConnect();
--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 
-//
+require_once __DIR__ . '/vendor/autoload.php';
+
+// $pdo = include_once __DIR__ . '/db.php';
+$pdo = require 'db.php';
+
+
+$sql = "SELECT * FROM users";
+$stmt = $pdo->query($sql);
+
+$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+dump($users);
